2c2bb16726e3575be278b3ca1071e2f2b1e2cfba
Sushytska <valia.sushytska@gmail.com>
2025-08-12 15:49:17 +0200

    Added small modifications to config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'; // Importing dotenv to manage environment variables
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config(); // Load environment variables from .env file to process.env
 
 interface Config {
     port: number; // Port number for the server
@@ -11,7 +11,7 @@ interface Config {
 const config: Config = {
     port: Number(process.env.PORT) || 3000, // Default to 3000 if PORT is not set
     nodeEnv: process.env.NODE_ENV || 'development', // Default to 'development' if NODE_ENV is not set
-    dbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017', // Default MongoDB URI
+    dbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/diary', // Default MongoDB URI
 };
 
 export default config; // Exporting the config object to be used in other parts of the application
